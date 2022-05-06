914dec21a0f6fc0cf67c6b4257c6c39fb1a81513
kilderMad <costa.kilder@live.com>
2022-05-06 14:33:47 -0300

    validaçao do código e CEP

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,4 +1,7 @@
 class Warehouse < ApplicationRecord
     validates :name, :description, :cep, :area, :city, :address, presence: true
-    validates :code, length: { is: 3 }
+    validates :code, length: { is: 3 }, uniqueness: true
+    validates :cep, format: { with: /\d{5}-?\d{3}/ }
+    
 end
+
--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -1,5 +1,80 @@
 require 'rails_helper'
 
 RSpec.describe Warehouse, type: :model do
-  
+  describe '#valid?' do
+    it 'quando nome é vazio' do
+        warehouse =Warehouse.new(name: '', code: 'RCF', address: 'Endereço',
+            cep: '12500-000', city: 'Rio', area: 100000,
+            description: 'alguma')
+
+        expect(warehouse.valid?).to eq false
+    end
+
+    it 'quando codigo é vazio' do
+        warehouse =Warehouse.new(name: 'Recife', code: '', address: 'Endereço',
+            cep: '12500-000', city: 'Rio', area: 100000,
+            description: 'alguma')
+
+        expect(warehouse.valid?).to eq false
+    end
+
+    it 'quando address é vazio' do
+        warehouse =Warehouse.new(name: 'Recife', code: 'RCF', address: '',
+            cep: '12500-000', city: 'Rio', area: 100000,
+            description: 'alguma')
+
+        expect(warehouse.valid?).to eq false
+    end
+
+    it 'quando cep é vazio' do
+        warehouse =Warehouse.new(name: 'Recife', code: 'RCF', address: 'Endereço',
+            cep: '', city: 'Rio', area: 100000,
+            description: 'alguma')
+
+        expect(warehouse.valid?).to eq false
+    end
+
+    it 'quando cidade é vazio' do
+        warehouse =Warehouse.new(name: 'Recife', code: 'RCF', address: 'Endereço',
+            cep: '12500-000', city: '', area: 100000,
+            description: 'alguma')
+
+        expect(warehouse.valid?).to eq false
+    end
+
+    it 'quando area é vazio' do
+        warehouse =Warehouse.new(name: 'Recife', code: 'RCF', address: 'Endereço',
+            cep: '12500-000', city: 'Rio', area: '',
+            description: 'alguma')
+
+        expect(warehouse.valid?).to eq false
+    end
+
+    it 'quando descrição é vazio' do
+        warehouse =Warehouse.new(name: 'Recife', code: 'RCF', address: 'Endereço',
+            cep: '12500-000', city: 'Rio', area: 100000,
+            description: '')
+
+        expect(warehouse.valid?).to eq false
+    end
+
+    it 'quando o codigo ja existe' do
+    first_warehouse =Warehouse.create(name: 'Rio', code: 'RIO', address: 'Endereço',
+        cep: '12500-000', city: 'Rio', area: 100000,
+        description: 'alguma')
+    second_warehouse =Warehouse.new(name: 'London', code: 'RIO', address: 'Av. Street',
+        cep: '40500-000', city: 'London', area: 200000,
+        description: 'paralelepipedo')
+
+    expect(second_warehouse.valid?).to eq false
+    end
+
+    it 'quando o cep nao é valido' do
+        warehouse =Warehouse.new(name: 'London', code: 'RIO', address: 'Av. Street',
+            cep: '400-000', city: 'London', area: 200000,
+            description: 'paralelepipedo')
+    
+        expect(warehouse.valid?).to eq false
+    end
+  end
 end
