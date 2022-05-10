1a82dccbe212d776f1b2ceb6f85487c7a377b551
kilderMad <costa.kilder@live.com>
2022-05-10 14:42:23 -0300

    mais validaçoes

--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -1,4 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe Supplier, type: :model do
+    describe '#valid?' do
+        it 'quando nome é vazio' do
+            supplier = Supplier.new(fantasy_name: '', company_name: 'CT Technology', cnpj: '9663123000109', address: 'Rua vírginio campos, 123',
+                email: 'cibertech@company.com.br', phone: '81 981316988')
+
+            expect(supplier.valid?).to eq false
+        end
+
+        it 'quando razao social é vazio' do
+            supplier = Supplier.new(fantasy_name: 'CiberTech', company_name: '', cnpj: '9663123000109', address: 'Rua vírginio campos, 123',
+                email: 'cibertech@company.com.br', phone: '81 981316988')
+
+            expect(supplier.valid?).to eq false
+        end
+
+        it 'quando CNPJ é vazio' do
+            supplier = Supplier.new(fantasy_name: 'CiberTech', company_name: 'CT Technology', cnpj: '', address: 'Rua vírginio campos, 123',
+                email: 'cibertech@company.com.br', phone: '81 981316988')
+
+            expect(supplier.valid?).to eq false
+        end
+
+        it 'quando email é vazio' do
+            supplier = Supplier.new(fantasy_name: 'CiberTech', company_name: 'CT Technology', cnpj: '9663123000109', address: 'Rua vírginio campos, 123',
+                email: '', phone: '81 981316988')
+
+            expect(supplier.valid?).to eq false
+        end
+
+        it 'quando CNPJ nao possui 13 digitos' do
+            supplier = Supplier.new(fantasy_name: 'CiberTech', company_name: 'CT Technology', cnpj: '966312000109', address: 'Rua vírginio campos, 123',
+                email: 'cibertech@company.com.br', phone: '81 981316988')
+
+            expect(supplier.valid?).to eq false
+        end
+
+        it 'quando CNPJ ja existe' do
+            f_supplier = Supplier.create!(fantasy_name: 'CiberTech', company_name: 'CT Technology', cnpj: '9663120001095', address: 'Rua vírginio campos, 123',
+                email: 'cibertech@company.com.br', phone: '81 981316988')
+            s_supplier = Supplier.new(fantasy_name: 'ChTech', company_name: 'CHGF Technology', cnpj: '9663120001095', address: 'Rua hongro fibonato, 567',
+                email: 'gthj@company.com.br', phone: '81 666316988')
+
+            expect(s_supplier.valid?).to eq false
+        end
+    end
 end
