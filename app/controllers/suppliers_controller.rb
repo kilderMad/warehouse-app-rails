ed4177c2b2fbc790f91e2e21b429fba418398bf9
kilderMad <costa.kilder@live.com>
2022-05-10 15:36:02 -0300

    editar supplier

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -22,4 +22,19 @@ class SuppliersController < ApplicationController
             render :new
         end
     end
+
+    def edit
+        @supplier = Supplier.find(params[:id])
+    end
+
+    def update
+        @supplier = Supplier.find(params[:id])
+        supplier_params = params.require(:supplier).permit(:fantasy_name, :company_name, :cnpj, :email, :phone, :address)
+        if @supplier.update(supplier_params)
+            redirect_to supplier_path(params[:id]), notice: 'Fornecedor editado com sucesso'
+        else
+            flash.now[:notice] = 'Falha ao editar'
+            render :edit
+        end
+    end
 end
